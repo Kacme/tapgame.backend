51a6ea0f4095c9af719876350387b3427d99635c
KLEBER AUGUSTO COSTA MORENO <kleber.acmoreno@senacsp.edu.br>
2022-08-18 22:10:16 -0300

    alterado info e score

---
+++ src/routes.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const UserController = require('./controllers/UserController');
+const router = express.Router();
+
+router.get('/ping', (req, res) => {
+    res.json({pong: true});
+});
+
+router.post('/signin', UserController.signin);
+router.post('/signup', UserController.signup);
+router.put('/user/:id', UserController.update);
+router.get('/user/:nick', UserController.info);
+router.put('/user/:id/timegame/:timeGame', UserController.timeGame);
+router.put('/user/:nick/score/:score', UserController.score);
+router.get('/ranking/:qtd', UserController.ranking);
+router.get('/highscore', UserController.highScore);
+
+module.exports = router;
